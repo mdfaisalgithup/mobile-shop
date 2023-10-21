c21d1d684408db6f6525d3c4abdca7b32ae864c6
mdfaisalgithupe <mdfaisald0706@gmail.com>
2023-10-21 15:47:43 +0600

    The wrong page has been changed in every way

--- src/Error/ErrorPage.jsx
+++ src/Error/ErrorPage.jsx
@@ -8,7 +8,7 @@ const ErrorPage = () => {
        <div>
        <h2 className="font-bold text-orange-600 xl:text-[25px] my-2 lg:text-lg text-[12px]">You have gone to the wrong page, come to the right page</h2>
             <h2 className="font-semibold">404 Not Found Page</h2>
-            <Link to="/"><button className="bg-black px-6 py-2 my-2 rounded-md font-bold text-white">Back</button></Link>
+            <Link to="/"><button className="bg-black px-6 py-2 my-3 rounded-md font-bold text-white">Back</button></Link>
        </div>
 
         </div>
